26a5df7a45cce7a19d46370ee9d0beb4785a6a7f
Michael Marais <michael@moondew.io>
2016-12-16 10:43:38 -0600

    Make some mobile changes.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -8,10 +8,10 @@ $(".first").stick_in_parent();
 
 $(".second").stick_in_parent()
     .on("sticky_kit:stick", function(e) {
-        $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
+        // $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
     })
     .on("sticky_kit:unstick", function(e) {
-        $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
+        // $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
     });
 $(".third").stick_in_parent()
     .on("sticky_kit:stick", function(e) {
@@ -27,4 +27,30 @@ $(".third").stick_in_parent()
     });
 $(".fourth .logoFg").stick_in_parent({
     offset_top: 200
+});
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+jQuery(document).ready(function(){
+    if( !isMobile.any() ){
+        $(window).stellar();
+    }
 });
