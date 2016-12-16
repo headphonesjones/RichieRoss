dc87ea18a63346c805bbb9ca4d005e318b46bcc7
Michael Marais <michael@moondew.io>
2016-12-16 12:12:24 -0600

    Make new mobile fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,30 +25,35 @@ var isMobile = {
 jQuery(document).ready(function(){
     if( !isMobile.any() ){
         $(window).stellar();
+
+        $(".first").stick_in_parent();
+
+        $(".second").stick_in_parent()
+            .on("sticky_kit:stick", function(e) {
+                $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
+            })
+            .on("sticky_kit:unstick", function(e) {
+                $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
+            });
+
+        $(".third").stick_in_parent()
+            .on("sticky_kit:stick", function(e) {
+                $(".third .columns").addClass('hide');
+                $(".fourth .logoFg").addClass('show');
+                $(".coolGlasses").addClass('hide');
+            })
+            .on("sticky_kit:unstick", function(e) {
+                $(".third .columns").removeClass('hide');
+                $(".fourth .logoFg").removeClass('show');
+                $(".coolGlasses").removeClass('hide');
+            });
+
+        $(".fourth .logoFg").stick_in_parent({
+            offset_top: 200
+        });
+    } else {
+        $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
     }
 });
 
-$(".first").stick_in_parent();
 
-$(".second").stick_in_parent()
-    .on("sticky_kit:stick", function(e) {
-        // $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
-    })
-    .on("sticky_kit:unstick", function(e) {
-        // $(".coolGlasses").attr('src','/dist/images/MOVIE_7-01.gif');
-    });
-$(".third").stick_in_parent()
-    .on("sticky_kit:stick", function(e) {
-        $(".third .columns").addClass('hide');
-        $(".fourth .logoFg").addClass('show');
-        $(".coolGlasses").addClass('hide');
-
-    })
-    .on("sticky_kit:unstick", function(e) {
-        $(".third .columns").removeClass('hide');
-        $(".fourth .logoFg").removeClass('show');
-        $(".coolGlasses").removeClass('hide');
-    });
-$(".fourth .logoFg").stick_in_parent({
-    offset_top: 200
-});
